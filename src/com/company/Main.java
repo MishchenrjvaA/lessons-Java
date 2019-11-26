a92341e567a9afa5af495b896d07f7ef3c9fc7b0
MishchenrjvaA <marta_nizhin2011@ukr.net>
2019-11-26 20:48:26 +0300

    create comment

---
+++ src/com/company/Main.java
@@ -0,0 +1,72 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	логические операторы:
+	>, <, >=, <=,
+	== - равенство;
+	!= - проверка на неравенство;
+	&& - логическое и;
+	||  - логическое или;
+	 */
+	int age = 21;
+	if (age>=18) {
+	    System.out.println("доступ разрешен");
+        } else {
+        System.out.println("доступ запрещен");
+        int a = 10, b = 20, c = 15;
+        if (a > b && a > c) {
+            System.out.println(a);
+        } else if (b > a && b > c) ;{
+            System.out.println(b);
+        } else
+    }
+
+        }
+        int month = 18;
+	    switch (month)  {
+            case 1:
+                System.out.println("январь");
+	    break;
+            case 2:
+                System.out.println("февраль");
+                break;
+            case 3:
+                System.out.println("март");
+                break;
+            case 4:
+                System.out.println("апрель");
+                break;
+            case 5:
+                System.out.println("май");
+                break;
+            case 6:
+                System.out.println("июнь");
+                break;
+            case 7:
+                System.out.println("июль");
+                break;
+            case 8:
+                System.out.println("август");
+                break;
+            case 9:
+                System.out.println("сентябрь");
+                break;
+            case 10:
+                System.out.println("октябрь");
+                break;
+            case 11:
+                System.out.println("ноябрь");
+                break;
+            case 12:
+                System.out.println("декабрь");
+                break;
+            default:
+                System.out.println("такого месяца нет");
+        }
+        // оператор %
+                System.out.println(5 % 3);
+    }
+
