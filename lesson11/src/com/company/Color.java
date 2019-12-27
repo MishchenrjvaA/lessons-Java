88ccbf14126cb2279cd0cfc64c1ce07adeaf9f1e
MishchenrjvaA <marta_nizhin2011@ukr.net>
2019-12-27 21:19:30 +0300

    lesson11

--- lesson11/src/com/company/Color.java
+++ lesson11/src/com/company/Color.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public enum Color {
+    // private только
+    //
+    RED("rgb(255,0,0)"),
+    Green("rgb(0,255,0)"),
+    BLACK("rgb(0,0,0)");
+    private String value;
+
+
+    Color(String value){
+        this.value = value;
+
+    }
+     public String getValue(){
+        return this.value = value;
+     }
 }
--- lesson11/src/com/company/Language.java
+++ lesson11/src/com/company/Language.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public enum Language {
+    // объекты enum-a
+    //все названия полей с большой буквы
+    RU,
+    EN,
+    BY,
+    UA;
+
+
 }
